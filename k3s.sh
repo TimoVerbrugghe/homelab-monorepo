1fa6f37702173a6caee269c81c10116578508fc2
Timo Verbrugghe <timo@hotmail.be>
2022-10-15 21:25:37 +0000

    Finalizing moving over to k3s helmCharts

--- k3s.sh
+++ k3s.sh
@@ -8,12 +8,6 @@ curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - server \
   --disable traefik \
   --tls-san 192.168.0.20
 
-k3sup install \
-	--local \
-	--cluster \
-	--tls-san 192.168.0.20 \
-	--k3s-extra-args="--disable servicelb --disable traefik"
-
 ## Install kube-vip
 ## See https://kube-vip.io/docs/installation/daemonset/#kube-vip-as-ha-load-balancer-or-both for updated instructions on how to install as daemonset
 
@@ -39,44 +33,29 @@ kube-vip manifest daemonset \
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.6/config/manifests/metallb-native.yaml
 kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/metallb/resources.yaml
 
-## Create namespaces for cert-manager & cattle-system
-kubectl create namespace cattle-system
-kubectl create namespace cert-manager
-
 ## Installing helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 ## Install cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
 
-helm install cert-manager jetstack/cert-manager \
- --namespace cert-manager \
- --create-namespace \
- --version v1.9.1 \
- -f /home/timoubuntuserver/HomeServerKubernetes/cert-manager/cert-manager-helm-values.yaml
+kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/cert-manager/cert-manager-deployment.yaml
 
 ## Install kubed so that secrets can be synced across all namespaces (f.e. the certificates that cert-manager will generate)
-helm repo add appscode https://charts.appscode.com/stable/
-helm repo update
-helm install kubed appscode/kubed \
-  --namespace kube-system
+kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/kubed/kubed-deployment.yaml
 
+# check deployment
 kubectl get deployment --namespace kubed -l "app.kubernetes.io/name=kubed,app.kubernetes.io/instance=kubed"
 
 ## Get certificates
-kubectl apply \
-  -f /home/timoubuntuserver/HomeServerKubernetes/cert-manager/issuers/
+kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/cert-manager/secrets
+kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/cert-manager/certificates
 
 ## Install traefik
-helm repo add traefik https://helm.traefik.io/traefik
-helm repo update
-helm install traefik traefik/traefik -f /home/timoubuntuserver/HomeServerKubernetes/traefik/traefik-helm-values.yaml
+kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/traefik/traefik-deployment.yaml
 
 ## Install rancher
-helm install rancher rancher-stable/rancher \
-  --namespace cattle-system \
-  --set hostname=ranchertest2.timo.be \
-  --set bootstrapPassword=admin
+kubectl apply -f /home/timoubuntuserver/HomeServerKubernetes/rancher/rancher-deployment.yaml
 
 ## Check deployment
-    kubectl -n cattle-system rollout status deploy/rancher
+kubectl -n cattle-system rollout status deploy/rancher
