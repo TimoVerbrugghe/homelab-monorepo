604e7c46cb3d02fff5740a62a9be5d50f9a00a18
Timo Verbrugghe <tverbrugghe@salesforce.com>
2023-04-24 13:22:22 +0200

    Adding dns server to docker command to make sure dns challenge works

--- docker/certbot-renewal.sh
+++ docker/certbot-renewal.sh
@@ -2,6 +2,7 @@
 docker run --rm \
     --name certbot \
     -v "/etc/letsencrypt:/etc/letsencrypt" \
+    --dns 8.8.8.8 \
     certbot/dns-cloudflare \
     certonly --non-interactive --keep-until-expiring -m timo@hotmail.be --agree-tos --no-eff-email \
     --dns-cloudflare --dns-cloudflare-propagation-seconds 20 --dns-cloudflare-credentials /etc/letsencrypt/cloudflare.ini \
