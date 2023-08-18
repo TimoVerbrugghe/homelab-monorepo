c0b3d560a2c41c7885636c8415544a7c03e20014
Timo Verbrugghe <timo@hotmail.be>
2023-08-18 10:47:14 +0200

    Moving all docker containers to docker compose

---
+++ ansible/install-proxmox-lxc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Download an LXC container, create a new one and start it
+
+# This environment variable needs to be set because one of the tasks is a file lookup on an url which triggers a bug with thread workers in python
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES 
+
+# Hosts should be put under the "proxmox" category
+
+ansible-playbook \
+     -i inventory/hosts.ini \
+     playbooks/proxmox-lxc.yml
--- ansible/install-ssh-keys.sh
+++ ansible/install-ssh-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ansible-playbook -vvv \
-     -i 192.168.0.20, \
+     -i ulrich.timo.local, \
      -e "ansible_user=root ansible_password=proxmox github_username=TimoVerbrugghe" \
      playbooks/install-ssh-keys.yml
