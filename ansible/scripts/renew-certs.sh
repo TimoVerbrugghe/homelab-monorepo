d109d9d28d01be26735de1433bd0ee481ba394f6
Timo Verbrugghe <timo@hotmail.be>
2023-08-31 22:12:22 +0200

    Define volume for homebridge

--- ansible/scripts/renew-certs.sh
+++ ansible/scripts/renew-certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ansible-playbook \
     -i ../inventory/hosts.yaml \
-    --limit "aelita.home.timo.be" \
+    --limit aelita.home.timo.be,ulrich.home.timo.be,odd.home.timo.be \
     ../playbooks/certbot-renewal.yml
