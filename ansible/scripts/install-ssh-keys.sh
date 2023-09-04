d341de75a27f1bd1f29384c248af35730facdd51
Timo Verbrugghe <timo@hotmail.be>
2023-09-05 00:07:49 +0200

    Ansible cleanup

--- ansible/scripts/install-ssh-keys.sh
+++ ansible/scripts/install-ssh-keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ansible-playbook \
      -i ../inventory/hosts.yaml \
-     --limit sectorfive.home.timo.be,localhost \
+     --limit mountainsector.local.timo.be,localhost \
      -e "ansible_user=root ansible_password=proxmox github_username=TimoVerbrugghe" \
      ../playbooks/install-ssh-keys.yml
