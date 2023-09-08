6aed4b33234a892cdffbaa8ca079f5603a24fcd7
Timo Verbrugghe <timo@hotmail.be>
2023-09-08 12:49:47 +0200

    Define specific IP addresses for netboot & airsane

--- ansible/scripts/configure-gamingvm.sh
+++ ansible/scripts/configure-gamingvm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This environment variable is necessary to avoid fork errors in ansible 
+# For more information, see https://github.com/ansible/ansible/issues/76322
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
 ansible-playbook \
      -i ../inventory/hosts.yaml \
      ../playbooks/gamingvm-setup.yml
