3c821acb3b6dc815c0959b26149609c3e3a09e96
Timo Verbrugghe <timo@hotmail.be>
2023-09-11 19:03:53 +0200

    Update iSCSI setup

--- ansible/roles/configure-gamingvm/scripts/iscsi-setup.ps1
+++ ansible/roles/configure-gamingvm/scripts/iscsi-setup.ps1
@@ -4,4 +4,4 @@ Set-Service -Name MSiSCSI -StartupType Automatic
 
 # Connect to Truenas & mount scsi drive
 New-IscsiTargetPortal -TargetPortalAddress "10.10.10.2"
-Connect-IscsiTarget -NodeAddress "iscsi.local.timo.be:windowsvm"
+Connect-IscsiTarget -NodeAddress "iscsi.local.timo.be:windowsvm" -IsPersistent $true
