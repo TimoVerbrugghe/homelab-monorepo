b92f3d0efab663fb0192449dc4aad7d7aaa3eec0
TimoVerbrugghe <timo@hotmail.be>
2023-12-27 19:00:23 +0000

    Add k3stest host to k3scluster

--- ansible/scripts/deploy-k3scluster-vm.sh
+++ ansible/scripts/deploy-k3scluster-vm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ansible-playbook \
     -i ../inventory/hosts.yaml \
-    --limit k3scluster,localhost \
+    --limit k3stest.local.timo.be,localhost \
     ../playbooks/deploy-k3scluster.yml
