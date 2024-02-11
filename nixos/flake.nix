cb5bade0920705558991e4d8e71762538ff0d46b
TimoVerbrugghe <timo@hotmail.be>
2024-02-11 20:19:44 +0100

    Add inherit inputs to flake.nix to hopefully fix inputs issue in user.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -16,7 +16,7 @@
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs;
+        specialArgs = {inherit inputs;};
         modules = [
           ./machines/nixos/configuration.nix
         ];
--- nixos/machines/nixos/configuration.nix
+++ nixos/machines/nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ inputs, config, lib, pkgs, ssh-keys, ... }:
+{ config, lib, pkgs, ssh-keys, ... }:
 
 let 
 
@@ -13,7 +13,6 @@ let
   kernelParams = [
      "i915.enable_guc=2" # Enable Intel Quicksync
   ];
-  inherit (inputs) ssh-keys;
 
 in 
 
