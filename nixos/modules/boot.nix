d82af3447f6e7c429713f18342d77ffb1f782c11
TimoVerbrugghe <timo@hotmail.be>
2024-02-25 22:25:47 +0100

    Only store last 3 configurations

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -5,7 +5,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 3;
 
   # Making sure we're running latest linux kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
