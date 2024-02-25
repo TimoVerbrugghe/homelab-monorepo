6565f4f82492a954f903c6d03c1da2001ab197e2
TimoVerbrugghe <timo@hotmail.be>
2024-02-25 23:12:44 +0100

    Change inputs in installer-configuration.nix and change hardware-configuration.nix to btrfs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -23,18 +23,8 @@
       };
       iso-autoinstall = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
+        specialArgs = inputs;
         modules = [
-          # Import minimal ISO CD
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-
-          # Import tools (needed for certain options such as system.nixos-generate-config)
-          "${nixpkgs}/nixos/modules/installer/tools/tools.nix"
-
-          # Provide an initial copy of the NixOS channel so that the user
-          # doesn't need to run "nix-channel --update" first.
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-
           # Basic installer & install configuration
           ./iso-autoinstall/installer-configuration.nix
         ];
