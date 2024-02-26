a8b13ca3841a4eb3626123922c9c75332bb04663
TimoVerbrugghe <timo@hotmail.be>
2024-02-26 23:47:44 +0100

    Updates to ansible config

--- ansible/scripts/configure-nixos.sh
+++ ansible/scripts/configure-nixos.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-ansible-playbook \
+#!/bin/sh
+ansible-playbook -vvv \
      -i "10.10.10.23," \
      -e "tailscaleAuthkey=" \
      -e "nixosFlake=nixos" \
+     -e "ansible_username=nixos" \
+     -e "ansible_pass=nixos" \
+     -e "ansible_sudo_pass=nixos" \
      ../playbooks/configure-nixos.yml
