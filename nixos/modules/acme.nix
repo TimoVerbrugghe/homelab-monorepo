e17fa84f6565f235347707c07591a190c3955168
Timo Verbrugghe <timo@hotmail.be>
2024-03-25 17:53:05 +0100

    Switch from certbot to acme nixos options

---
+++ nixos/modules/acme.nix
@@ -0,0 +1,41 @@
+{ inputs, config, lib, pkgs, ... }:
+
+with lib;
+with types;
+
+let
+  acmeEmail = "timo@hotmail.be";
+in
+
+{
+
+  imports =[ 
+      # Include cloudflare API key file, you need to put this manually in your nixos install
+      /etc/nixos/cloudflare-apikey.nix
+  ];
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = acmeEmail;
+    defaults.environmentFile = "${pkgs.writeText "cloudflare-env" ''
+      CLOUDFLARE_DNS_API_TOKEN=${config.cloudflareApiKey}
+    ''}";
+    defaults.dnsProvider = "cloudflare";
+    defaults.dnsResolver = "1.1.1.1:53";
+    defaults.extraLegoFlags = [ # Store certificates in /etc/letsencrypt folder
+      "--path /etc/letsencrypt"
+    ];
+
+    certs = {
+      "dns.timo.be" = {
+      };
+      "home.timo.be" = {
+      };
+      "*.home.timo.be" = {
+      };
+    }
+  };
+
+
+}
+
