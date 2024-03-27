15fd1bfe8fd88959e8fb7ee31c7888446824fd0d
Timo Verbrugghe <timo@hotmail.be>
2024-03-27 13:14:51 +0100

    Add systemd service to resolve tailscale container restarts and some containers exiting on boot to default modules

--- nixos/modules/docker.nix
+++ nixos/modules/docker.nix
@@ -11,4 +11,41 @@
   environment.systemPackages = with pkgs; [
     docker-compose
   ];
+
+  ## Add this systemd service because docker keeps quitting containers on startup that I have connected with tailscale even with docker compose healthchecks -_-'
+
+  systemd.services.docker-container-restart = {
+    description = "Periodically restart Docker containers with specific error - cannot join network of running container";
+    after = [ "network-online.target" "docker.service" "docker.socket"];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    enable = true;
+    path = with pkgs; [
+      docker
+    ];
+    script = ''
+      docker ps -a --filter "status=exited" --format "{{.ID}}" | while read -r container_id; do
+        error=$(docker inspect --format "{{.State.Error}}" "$container_id")
+        if [[ "$error" == *"cannot join network of a non running container"* ]]; then
+          docker restart "$container_id"
+        fi
+      done
+    '';
+    serviceConfig = {
+      User = "root";
+      Group = "root";
+      Type = "oneshot";
+      restart = "no";
+    };
+    
+  };
+
+  systemd.timers.docker-container-restart = {
+    description = "Timer to periodically restart Docker containers";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*:0/3";  # Run every 3 minutes
+      Persistent = true;
+    };
+  };
 }
