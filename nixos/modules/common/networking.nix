59184a107466d0603ac81e7d876fbf0168c61662
Timo Verbrugghe <timo@hotmail.be>
2024-04-01 00:40:45 +0200

    Remove matter-server specific networking options

--- nixos/modules/common/networking.nix
+++ nixos/modules/common/networking.nix
@@ -9,16 +9,4 @@
     "net.core.wmem_default" = 25000000;
   };
 
-  # Disable ipv6 forwarding for better compatibility with matter server (see https://github.com/home-assistant-libs/python-matter-server/blob/main/README.md#requirements-to-communicate-with-thread-devices-through-thread-border-routers)
-
-  boot.kernel.sysctl = {
-    # Disabling ipv6 forwarding
-    "net.ipv6.conf.all.forwarding" = 0;
-
-    "net.ipv6.conf.eth0.accept_ra" = 1;
-    "net.ipv6.conf.eth0.accept_ra_rt_info_max_plen" = 64;
-    "net.ipv6.conf.macvlan0.accept_ra" = 1;
-    "net.ipv6.conf.macvlan0.accept_ra_rt_info_max_plen" = 64;
-  };
-
 }
