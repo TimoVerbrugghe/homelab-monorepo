388be027d48aadbb6b2931c8a3548872749be543
Timo Verbrugghe <timo@hotmail.be>
2024-03-31 19:30:08 +0200

    Typo fix

--- nixos/modules/common/networking.nix
+++ nixos/modules/common/networking.nix
@@ -19,6 +19,6 @@
     "net.ipv6.conf.eth0.accept_ra_rt_info_max_plen" = 64;
     "net.ipv6.conf.macvlan0.accept_ra" = 1;
     "net.ipv6.conf.macvlan0.accept_ra_rt_info_max_plen" = 64;
-  }
+  };
 
 }
