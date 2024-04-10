099817f801070c82c9455ba2c6cb2ed82490404f
Timo Verbrugghe <timo@hotmail.be>
2024-04-10 23:47:37 +0200

    Upgrade to es-de 3.0 (not available yet in nixpkgs)

--- nixos/machines/gamingserver/configuration.nix
+++ nixos/machines/gamingserver/configuration.nix
@@ -130,7 +130,8 @@ in
     # emulationstation-de
     ryujinx
 
-    (pkgs.callPackage ./emulationstation-de {})
+    # Current emulationstation-de is old (2.2.1), so temporarily building the package 3.0.1 myself using the commit https://github.com/NixOS/nixpkgs/pull/299298 until it's pulled
+    (pkgs.callPackage ./emulationstation-de/package.nix {})
   ];
 
   # Need this for emulationstation-de
