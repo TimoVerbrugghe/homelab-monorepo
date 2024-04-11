5034c7100a7b0680185b14bd441ed9db05979a65
Timo Verbrugghe <timo@hotmail.be>
2024-04-12 01:57:17 +0200

    Remove default icon setting using layout.js because it's just putting it as start.svg

--- nixos/machines/gamingserver/vapor-nixos-theme/Vapor-Nixos/look-and-feel/contents/layouts/org.kde.plasma.desktop-layout.js
+++ nixos/machines/gamingserver/vapor-nixos-theme/Vapor-Nixos/look-and-feel/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -7,8 +7,4 @@ for( var i = 0; i < desktopsArray.length; i++) {
     desktopsArray[i].wallpaperPlugin = 'org.kde.image';
     desktopsArray[i].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General");
     desktopsArray[i].writeConfig("Image", "/run/current-system/sw/share/plasma/desktoptheme/Vapor-Nixos/wallpapers/Steam Deck Logo Default.jpg");
-
-    // Set default application launcher icon
-    desktopsArray[i].currentConfigGroup = ["General"];
-    desktop.writeConfig("icon", "/run/current-system/sw/share/plasma/desktoptheme/Vapor-Nixos/icons/start.svg");
 }
