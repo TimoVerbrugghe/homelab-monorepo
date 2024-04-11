c937cd51f00401ad9146520dd452d68f67d1bd39
TimoVerbrugghe <timo@hotmail.be>
2024-04-12 00:14:00 +0200

    Changes after building vapor theme locally

--- nixos/machines/gamingserver/vapor-nixos-theme/look-and-feel/Vapor-Nixos/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++
@@ -1,4 +0,0 @@
-applet.wallpaperPlugin = 'org.kde.image'
-applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/run/current-system/sw/share/plasma/desktoptheme/Vapor-Nixos/wallpapers/Steam Deck Logo Default.jpg")
-applet.reloadConfig()
