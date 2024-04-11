7d0c68990843c0ef9851a8c2e3b836ba073cb57f
Timo Verbrugghe <timo@hotmail.be>
2024-04-12 01:47:31 +0200

    Moving from plasmoidsetupscripts just to a manually defined layout to see if that works

---
+++ nixos/machines/gamingserver/vapor-nixos-theme/Vapor-Nixos/look-and-feel/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,14 @@
+loadTemplate("org.kde.plasma.desktop.defaultPanel")
+
+var desktopsArray = desktopsForActivity(currentActivity());
+for( var i = 0; i < desktopsArray.length; i++) {
+
+    // Set default wallpaper
+    desktopsArray[i].wallpaperPlugin = 'org.kde.image';
+    desktopsArray[i].currentConfigGroup = new Array("Wallpaper", "org.kde.image", "General");
+    desktopsArray[i].writeConfig("Image", "/run/current-system/sw/share/plasma/desktoptheme/Vapor-Nixos/wallpapers/Steam Deck Logo Default.jpg");
+
+    // Set default application launcher icon
+    desktopsArray[i].currentConfigGroup = ["General"];
+    desktop.writeConfig("icon", "/run/current-system/sw/share/plasma/desktoptheme/Vapor-Nixos/icons/distributor-logo-steamdeck.svg");
+}
--- nixos/machines/gamingserver/vapor-nixos-theme/Vapor-Nixos/look-and-feel/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++
@@ -1,3 +0,0 @@
-applet.currentConfigGroup = ["General"]
-applet.writeConfig("icon", "/run/current-system/sw/share/plasma/desktoptheme/Vapor-Nixos/icons/distributor-logo-steamdeck.svg")
-applet.reloadConfig()
