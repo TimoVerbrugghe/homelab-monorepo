46444b88483fb80652c99150f4ccb8c4c51358c3
Timo Verbrugghe <timo@hotmail.be>
2024-04-12 23:57:02 +0200

    Enable low-latency audio

--- nixos/machines/gamingserver/sound.nix
+++ nixos/machines/gamingserver/sound.nix
@@ -2,8 +2,6 @@
 
 {
   # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -11,5 +9,35 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  # Enabling low-latency settings for pipewire - from nixos docs
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
+    context.properties = {
+      default.clock.rate = 48000;
+      default.clock.quantum = 32;
+      default.clock.min-quantum = 32;
+      default.clock.max-quantum = 32;
+    };
+  };
+
+  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
+    context.modules = [
+      {
+        name = "libpipewire-module-protocol-pulse";
+        args = {
+          pulse.min.req = "32/48000";
+          pulse.default.req = "32/48000";
+          pulse.max.req = "32/48000";
+          pulse.min.quantum = "32/48000";
+          pulse.max.quantum = "32/48000";
+        };
+      }
+    ];
+    stream.properties = {
+      node.latency = "32/48000";
+      resample.quality = 1;
+    };
+  };
+
 
 }
