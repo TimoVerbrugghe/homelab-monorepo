4944d784385c91d2b20f4252402a74b51e6b1ad1
Timo Verbrugghe <timo@hotmail.be>
2024-04-13 01:06:28 +0200

    Add realtime capabilities to user for crackling sound

--- nixos/machines/gamingserver/sound.nix
+++ nixos/machines/gamingserver/sound.nix
@@ -40,4 +40,33 @@
   };
 
 
+  # Add realtime capabilities to user to mitigate audio crackling
+  users = {
+    users.gamer.extraGroups = [ "realtime" ];
+    groups.realtime = { };
+  };
+  services.udev.extraRules = ''
+    KERNEL=="cpu_dma_latency", GROUP="realtime"
+  '';
+  security.pam.loginLimits = [
+    {
+      domain = "@realtime";
+      type = "-";
+      item = "rtprio";
+      value = 98;
+    }
+    {
+      domain = "@realtime";
+      type = "-";
+      item = "memlock";
+      value = "unlimited";
+    }
+    {
+      domain = "@realtime";
+      type = "-";
+      item = "nice";
+      value = -11;
+    }
+  ];
+
 }
