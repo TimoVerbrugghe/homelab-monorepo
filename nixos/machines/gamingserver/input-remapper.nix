3f2a8ea8815a4d6a0dacf1d520b2a83b56079341
Timo Verbrugghe <timo@hotmail.be>
2024-04-14 01:44:32 +0200

    Forgot to add action add to input-remapper

--- nixos/machines/gamingserver/input-remapper.nix
+++ nixos/machines/gamingserver/input-remapper.nix
@@ -6,7 +6,7 @@
 
   ## Udev rule for when Xbox controller gets plugged in
   services.udev.extraRules = ''
-    SUBSYSTEMS=="input", ATTRS{id/vendor}=="045e", ATTRS{id/product}=="028e", RUN+="${pkgs.systemd}/bin/systemctl --user restart input-remapper-autoload.service"
+    ACTION=="add", SUBSYSTEM=="input", ATTR{id/vendor}=="045e", ATTR{id/product}=="028e", RUN+="${pkgs.systemd}/bin/systemctl --user restart input-remapper-autoload.service"
   '';
 
   # Create input-remapper-autoload service that gets triggered by the udev rule so that input-remapper rules get reloaded again when controller gets plugged in again (because they stop when controller disconnects)
