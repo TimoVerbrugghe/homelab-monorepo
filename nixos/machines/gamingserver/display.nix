e92c0c07e182e7c02af5940b6408a33df01ace1a
Timo Verbrugghe <timo@hotmail.be>
2024-04-14 15:55:15 +0200

    Add plasmanotifyrc additional options that I forgot

--- nixos/machines/gamingserver/display.nix
+++ nixos/machines/gamingserver/display.nix
@@ -49,6 +49,38 @@
     # Notification settings
     "xdg/plasmanotifyrc" = {
       text = ''
+        [Applications][@other]
+        ShowPopups=false
+
+        [Applications][org.kde.konsole]
+        ShowBadges=false
+        ShowInHistory=false
+        ShowPopups=false
+
+        [Applications][org.kde.spectacle]
+        ShowBadges=false
+        ShowInHistory=false
+        ShowPopups=false
+
+        [Badges]
+        InTaskManager=false
+
+        [Jobs]
+        InNotifications=false
+        InTaskManager=false
+
+        [Notifications]
+        CriticalInDndMode=false
+        LowPriorityPopups=false
+
+        [Services][kaccess]
+        ShowInHistory=false
+        ShowPopups=false
+
+        [Services][plasma_workspace]
+        ShowInHistory=false
+        ShowPopups=false
+
         [Services][devicenotifications]
         ShowInHistory=false
         ShowPopups=false
--- nixos/machines/gamingserver/home.nix
+++ nixos/machines/gamingserver/home.nix
@@ -13,69 +13,6 @@
   home.stateVersion = "23.11";
 
   home.file = {
-    # ".config/plasmanotifyrc" = {
-    #   text = ''
-    #     [Services][devicenotifications]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][freespacenotifier]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][kcm_touchpad]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][ksmserver]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][kwalletd5]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][kwin]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][kwrited]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][networkmanagement]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][org.kde.kded.inotify]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][phonon]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][policykit1-kde]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][powerdevil]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][printmanager]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][proxyscout]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-
-    #     [Services][xdg-desktop-portal-kde]
-    #     ShowInHistory=false
-    #     ShowPopups=false
-    #   '';
-    # };
 
     ".steam/root/config/uioverrides/.keep" = {
       text = ''
