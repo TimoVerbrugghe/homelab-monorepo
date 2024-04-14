8f8172a5b1b8f87d6d8e17eae34bbd9d8d5d79f9
Timo Verbrugghe <timo@hotmail.be>
2024-04-14 16:06:03 +0200

    Syntax error fix

--- nixos/machines/gamingserver/home.nix
+++ nixos/machines/gamingserver/home.nix
@@ -17,7 +17,7 @@
     ".config/input-remapper-2" = {
       source = ".dotfiles/input-remapper-2";
       recursive = true;
-    }
+    };
 
     ".steam/root/config/uioverrides/.keep" = {
       text = ''
