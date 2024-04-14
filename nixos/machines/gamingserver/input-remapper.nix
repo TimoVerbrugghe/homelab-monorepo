1171d96335331e9e3fcbaca432c9d3a1d88d0556
Timo Verbrugghe <timo@hotmail.be>
2024-04-14 16:16:39 +0200

    Trying to rewrite input-remapper service to one in systemd-user

--- nixos/machines/gamingserver/input-remapper.nix
+++ nixos/machines/gamingserver/input-remapper.nix
@@ -8,5 +8,18 @@
     ## Enable built-in udev rules for when Xbox Controller is plugged in/connected through sunshine
     enableUdevRules = true;
   };
-  
+
+  systemd.user.services.input-remapper = {
+    path = with pkgs; [
+      input-remapper
+    ];
+    wantedBy = [ "graphical.target" ];
+    requires = ["dbus.service"];
+    after = ["dbus.service"];
+    serviceConfig = {
+      Type="dbus";
+      BusName="inputremapper.Control";
+      ExecStart="${pkgs.input-remapper}/bin/input-remapper-service";
+    };
+  };
 }
