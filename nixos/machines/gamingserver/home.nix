187208a459e8516bc17facd0496cdf63dc5f9cf4
Timo Verbrugghe <timo@hotmail.be>
2024-04-15 22:12:58 +0200

    Adding desktop shortcut files for RPCS3 to ES-DE

--- nixos/machines/gamingserver/home.nix
+++ nixos/machines/gamingserver/home.nix
@@ -54,7 +54,7 @@
     };
 
     # PS3 ROM File shortcuts for RPCS3
-    "test.desktop" = {
+    "ROMs/ps3/Ratchet and Clank® Future: Tools of Destruction™.desktop" = {
       text = ''
         [Desktop Entry]
         Encoding=UTF-8
@@ -68,5 +68,81 @@
         Icon=/home/gamer/.config/rpcs3/Icons/game_icons/NPUA80965/shortcut.png
       '';
     };
+
+    "ROMs/ps3/Ratchet & Clank Future： A Crack In Time.desktop" = {
+      text = ''
+      [Desktop Entry]
+      Encoding=UTF-8
+      Version=1.0
+      Type=Application
+      Terminal=false
+      Exec="${pkgs.rpcs3}/bin/.rpcs3-wrapped" --no-gui "%%RPCS3_GAMEID%%:NPUA80966"
+      Name=Ratchet & Clank Future: A Crack In Time
+      Categories=Application;Game
+      Comment=Ratchet & Clank Future: A Crack In Time
+      Icon=/home/gamer/.config/rpcs3/Icons/game_icons/NPUA80966/shortcut.png
+      '';
+    };
+
+    "ROMs/ps3/Ratchet & Clank® Future： Quest for Booty™.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Encoding=UTF-8
+        Version=1.0
+        Type=Application
+        Terminal=false
+        Exec="${pkgs.rpcs3}/bin/.rpcs3-wrapped" --no-gui "%%RPCS3_GAMEID%%:NPUA80145"
+        Name=Ratchet & Clank® Future: Quest for Booty™
+        Categories=Application;Game
+        Comment=Ratchet & Clank® Future: Quest for Booty™
+        Icon=/home/gamer/.config/rpcs3/Icons/game_icons/NPUA80145/shortcut.png
+      '';
+    };
+
+    "ROMs/ps3/Ratchet and Clank® Future： Tools of Destruction™.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Encoding=UTF-8
+        Version=1.0
+        Type=Application
+        Terminal=false
+        Exec="${pkgs.rpcs3}/bin/.rpcs3-wrapped" --no-gui "%%RPCS3_GAMEID%%:NPUA80965"
+        Name=Ratchet and Clank® Future: Tools of Destruction™
+        Categories=Application;Game
+        Comment=Ratchet and Clank® Future: Tools of Destruction™
+        Icon=/home/gamer/.config/rpcs3/Icons/game_icons/NPUA80965/shortcut.png
+      '';
+    };
+
+    "ROMs/ps3/Ratchet & Clank™： Going Commando.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Encoding=UTF-8
+        Version=1.0
+        Type=Application
+        Terminal=false
+        Exec="${pkgs.rpcs3}/bin/.rpcs3-wrapped" --no-gui "%%RPCS3_GAMEID%%:NPUA80644"
+        Name=Ratchet & Clank™: Going Commando
+        Categories=Application;Game
+        Comment=Ratchet & Clank™: Going Commando
+        Icon=/home/gamer/.config/rpcs3/Icons/game_icons/NPUA80644/shortcut.png
+      '';
+    };
+
+    "ROMs/ps3/Ratchet & Clank™： Up Your Arsenal.desktop" = {
+      text = ''
+        [Desktop Entry]
+        Encoding=UTF-8
+        Version=1.0
+        Type=Application
+        Terminal=false
+        Exec="${pkgs.rpcs3}/bin/.rpcs3-wrapped" --no-gui "%%RPCS3_GAMEID%%:NPUA80645"
+        Name=Ratchet & Clank™: Up Your Arsenal
+        Categories=Application;Game
+        Comment=Ratchet & Clank™: Up Your Arsenal
+        Icon=/home/gamer/.config/rpcs3/Icons/game_icons/NPUA80645/shortcut.png
+      '';
+    };
+
   };
 }
