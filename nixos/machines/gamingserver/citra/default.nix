7e07aac767c0cc5113539d92e2f5d2ce13b24a64
Timo Verbrugghe <timo@hotmail.be>
2024-04-18 23:05:53 +0200

    Wrong filename

--- nixos/machines/gamingserver/citra/default.nix
+++ nixos/machines/gamingserver/citra/default.nix
@@ -6,7 +6,7 @@
 let
   source = /etc/nixos/citra-unified-source.tar.xz;
 in {
-  nightly = qt6Packages.callPackage ./generic.nix rec {
+  nightly = qt6Packages.callPackage ./package.nix rec {
     pname = "citra-nightly";
     version = "2088";
 
