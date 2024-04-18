361c4c7c44dac2bfcfa6fd7549d9cecc75f05c24
Timo Verbrugghe <timo@hotmail.be>
2024-04-18 23:14:27 +0200

    removing nightly option

--- nixos/machines/gamingserver/citra/default.nix
+++ nixos/machines/gamingserver/citra/default.nix
@@ -6,11 +6,10 @@
 
 let
   source = /etc/nixos/citra-unified-source.tar.xz;
-in {
-  nightly = qt6Packages.callPackage ./package.nix rec {
+in
+  qt6Packages.callPackage ./package.nix rec {
     pname = "citra-nightly";
     version = "2088";
 
     src = source;
-  };
-}
+  }
