68e42ea2e87933d1db73f5514c0f8e7af523927a
Timo Verbrugghe <timo@hotmail.be>
2024-04-18 23:21:28 +0200

    qt6Packages is part of pkgs

--- nixos/machines/gamingserver/emulators.nix
+++ nixos/machines/gamingserver/emulators.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, qt6Packages, ... }:
+{ config, pkgs, ... }:
 
 {
 
-  environment.systemPackages = with pkgs qt6Packages; [
+  environment.systemPackages = with pkgs; [
     # Uncomment until https://github.com/NixOS/nixpkgs/pull/303494 is pulled
     # retroarchFull
     duckstation
@@ -22,7 +22,7 @@
     # Current emulationstation-de is old (2.2.1), so temporarily building the package 3.0.1 myself using the commit https://github.com/NixOS/nixpkgs/pull/299298 until it's pulled
     (pkgs.callPackage ./emulationstation-de/package.nix {})
 
-    (qt6Packages.callPackage ./citra/package.nix rec {
+    (pkgs.qt6Packages.callPackage ./citra/package.nix rec {
       pname = "citra-nightly";
       version = "2088";
 
