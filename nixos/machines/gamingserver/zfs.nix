4223cefaaa9b27d53c49c59950d89ad2e0afeab3
Timo Verbrugghe <timo@hotmail.be>
2024-04-28 13:50:33 +0200

    Add networking.hostId for gamingserver

--- nixos/machines/gamingserver/zfs.nix
+++ nixos/machines/gamingserver/zfs.nix
@@ -7,5 +7,8 @@
   services.zfs.trim.enable = true;
   boot.supportedFilesystems = [ "zfs" ];
 
+  # Set networking.hostId because ZFS needs this to prevent zfs pool from being mounted on the wrong machine. Host id is derived from the systemd machine id (head -c 8 /etc/machine-id)
+  networking.hostId = "e6b5d6a5";
+
   boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages; # Making sure we're running latest linux kernel that is ZFS compatible
 }
