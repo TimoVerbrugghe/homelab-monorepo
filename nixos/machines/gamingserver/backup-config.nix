2018119ce09bb233a224f7803dffa739e394f3aa
Timo Verbrugghe <timo@hotmail.be>
2024-04-29 00:35:38 +0200

    Only keep rom sync in backup config since I'll be using zfs snapshots

--- nixos/machines/gamingserver/backup-config.nix
+++ nixos/machines/gamingserver/backup-config.nix
@@ -9,160 +9,160 @@ in
 
 {
   ## Packages needed for backup
-  environment.systemPackages = with pkgs; [
-    gnutar
-    mount
-    rsync
-    umount
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   gnutar
+  #   mount
+  #   rsync
+  #   umount
+  # ];
 
-  systemd.services.backup-gamingserver = {
-    description = "Backup gamingserver core directories";
-    requires = [ "network.target" ];
-    script = ''
+  # systemd.services.backup-gamingserver = {
+  #   description = "Backup gamingserver core directories";
+  #   requires = [ "network.target" ];
+  #   script = ''
     
-    # Setting up backup & exclude directories
-      backup_dirs=(
-        "/home/gamer/ES-DE"
-        "/home/gamer/.config/Cemu"
-        "/home/gamer/.config/citra-emu"
-        "/home/gamer/.config/dolphin-emu"
-        "/home/gamer/.config/melonDS"
-        "/home/gamer/.config/mgba"
-        "/home/gamer/.config/PCSX2"
-        "/home/gamer/.config/ppsspp"
-        "/home/gamer/.config/retroarch"
-        "/home/gamer/.config/rpcs3"
-        "/home/gamer/.config/Ryujinx"
-        "/home/gamer/.config/sunshine"
-        "/home/gamer/.local/share/Cemu"
-        "/home/gamer/.local/share/citra-emu"
-        "/home/gamer/.local/share/dolphin-emu"
-        "/home/gamer/.local/share/duckstation"
-        "/home/gamer/.local/share/Steam"
-        "/home/gamer/.local/share/xemu"
-      )
+  #   # Setting up backup & exclude directories
+  #     backup_dirs=(
+  #       "/home/gamer/ES-DE"
+  #       "/home/gamer/.config/Cemu"
+  #       "/home/gamer/.config/citra-emu"
+  #       "/home/gamer/.config/dolphin-emu"
+  #       "/home/gamer/.config/melonDS"
+  #       "/home/gamer/.config/mgba"
+  #       "/home/gamer/.config/PCSX2"
+  #       "/home/gamer/.config/ppsspp"
+  #       "/home/gamer/.config/retroarch"
+  #       "/home/gamer/.config/rpcs3"
+  #       "/home/gamer/.config/Ryujinx"
+  #       "/home/gamer/.config/sunshine"
+  #       "/home/gamer/.local/share/Cemu"
+  #       "/home/gamer/.local/share/citra-emu"
+  #       "/home/gamer/.local/share/dolphin-emu"
+  #       "/home/gamer/.local/share/duckstation"
+  #       "/home/gamer/.local/share/Steam"
+  #       "/home/gamer/.local/share/xemu"
+  #     )
 
-      # Exclude the steamapps subdirectory
-      exclude_dirs=(
-        "/home/gamer/.local/share/Steam/steamapps"
-      )
+  #     # Exclude the steamapps subdirectory
+  #     exclude_dirs=(
+  #       "/home/gamer/.local/share/Steam/steamapps"
+  #     )
 
-      # Mounting NFS share
-      temp_dir_mount=$(mktemp -d)
-      ${pkgs.mount}/bin/mount -t nfs ${nasURL}:${nasBackupDir} $temp_dir_mount
+  #     # Mounting NFS share
+  #     temp_dir_mount=$(mktemp -d)
+  #     ${pkgs.mount}/bin/mount -t nfs ${nasURL}:${nasBackupDir} $temp_dir_mount
 
-      temp_dir_tar=$(mktemp -d)
-      output_tar="$temp_dir_tar/gamer_backup.tar"
+  #     temp_dir_tar=$(mktemp -d)
+  #     output_tar="$temp_dir_tar/gamer_backup.tar"
 
-      # Creating tar archive
-      echo "Starting backup"
-      ${pkgs.gnutar}/bin/tar --verbose --exclude="''${exclude_dirs[@]}" -cf "$output_tar" "''${backup_dirs[@]}"
+  #     # Creating tar archive
+  #     echo "Starting backup"
+  #     ${pkgs.gnutar}/bin/tar --verbose --exclude="''${exclude_dirs[@]}" -cf "$output_tar" "''${backup_dirs[@]}"
 
-      # Check if the tar creation was successful
-      if [ $? -eq 0 ]; then
-        ${pkgs.rsync}/bin/rsync -avhP $output_tar $temp_dir_mount/gamer_backup.tar
+  #     # Check if the tar creation was successful
+  #     if [ $? -eq 0 ]; then
+  #       ${pkgs.rsync}/bin/rsync -avhP $output_tar $temp_dir_mount/gamer_backup.tar
 
-        # Check if rsync was successful
-        if [ $? -eq 0 ]; then
-          ${pkgs.umount}/bin/umount $temp_dir_mount
-          echo "Backup completed successfully. Archive saved at: $output_tar"
-        else
-          echo "Failed moving backup archive to NAS share"
-        fi
+  #       # Check if rsync was successful
+  #       if [ $? -eq 0 ]; then
+  #         ${pkgs.umount}/bin/umount $temp_dir_mount
+  #         echo "Backup completed successfully. Archive saved at: $output_tar"
+  #       else
+  #         echo "Failed moving backup archive to NAS share"
+  #       fi
 
-      else
-        echo "Backup failed."
-      fi
+  #     else
+  #       echo "Backup failed."
+  #     fi
 
-      # Clean Up
-      rm -rf $temp_dir_mount
-      rm -rf $temp_dir_tar
+  #     # Clean Up
+  #     rm -rf $temp_dir_mount
+  #     rm -rf $temp_dir_tar
 
-    '';
+  #   '';
 
-    serviceConfig = {
-      Type = "oneshot";
-    };
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #   };
 
-  };
+  # };
 
-  # Backup gamingserver config directories on a monthly basis
-  systemd.timers.backup-gamingserver = {
-    description = "Backup gamingserver config directories on a monthly basis";
-    timerConfig = {
-      OnCalendar = "monthly";
-      Unit = "backup-gamingserver.service";
-    };
-  };
+  # # Backup gamingserver config directories on a monthly basis
+  # systemd.timers.backup-gamingserver = {
+  #   description = "Backup gamingserver config directories on a monthly basis";
+  #   timerConfig = {
+  #     OnCalendar = "monthly";
+  #     Unit = "backup-gamingserver.service";
+  #   };
+  # };
 
-  systemd.services.restore-gamingserver = {
-    description = "Restore gamingserver core directories";
-    requires = [ "network.target" ];
-    script = ''
-      # Temporary directory for mounting nfs share
-      temp_dir_mount=$(mktemp -d)
-      ${pkgs.mount}/bin/mount -t nfs ${nasURL}:${nasBackupDir} $temp_dir_mount
+  # systemd.services.restore-gamingserver = {
+  #   description = "Restore gamingserver core directories";
+  #   requires = [ "network.target" ];
+  #   script = ''
+  #     # Temporary directory for mounting nfs share
+  #     temp_dir_mount=$(mktemp -d)
+  #     ${pkgs.mount}/bin/mount -t nfs ${nasURL}:${nasBackupDir} $temp_dir_mount
 
-      # Input tar file to restore
-      input_tar="$temp_dir_mount/gamer_backup.tar"
+  #     # Input tar file to restore
+  #     input_tar="$temp_dir_mount/gamer_backup.tar"
 
-      # Temp dir to put tar file
-      temp_dir_tar=$(mktemp -d)
+  #     # Temp dir to put tar file
+  #     temp_dir_tar=$(mktemp -d)
 
-      # Rsync tar file from NAS server to local machine
-      ${pkgs.rsync}/bin/rsync -avhP $input_tar $temp_dir_tar/gamer_backup.tar
+  #     # Rsync tar file from NAS server to local machine
+  #     ${pkgs.rsync}/bin/rsync -avhP $input_tar $temp_dir_tar/gamer_backup.tar
 
-      # Temporary directory for extraction
-      temp_dir_extract=$(mktemp -d)
+  #     # Temporary directory for extraction
+  #     temp_dir_extract=$(mktemp -d)
 
-      # Extract the tar archive
-      ${pkgs.gnutar}/bin/tar -xf "$temp_dir_tar/gamer_backup.tar" -C "$temp_dir_extract"
+  #     # Extract the tar archive
+  #     ${pkgs.gnutar}/bin/tar -xf "$temp_dir_tar/gamer_backup.tar" -C "$temp_dir_extract"
 
-      # Restore each directory to its original location
-      restore_dirs=(
-        "/home/gamer/ES-DE"
-        "/home/gamer/.config/Cemu"
-        "/home/gamer/.config/citra-emu"
-        "/home/gamer/.config/dolphin-emu"
-        "/home/gamer/.config/melonDS"
-        "/home/gamer/.config/mgba"
-        "/home/gamer/.config/PCSX2"
-        "/home/gamer/.config/ppsspp"
-        "/home/gamer/.config/retroarch"
-        "/home/gamer/.config/rpcs3"
-        "/home/gamer/.config/Ryujinx"
-        "/home/gamer/.config/sunshine"
-        "/home/gamer/.local/share/Cemu"
-        "/home/gamer/.local/share/citra-emu"
-        "/home/gamer/.local/share/dolphin-emu"
-        "/home/gamer/.local/share/duckstation"
-        "/home/gamer/.local/share/Steam"
-        "/home/gamer/.local/share/xemu"
-      )
+  #     # Restore each directory to its original location
+  #     restore_dirs=(
+  #       "/home/gamer/ES-DE"
+  #       "/home/gamer/.config/Cemu"
+  #       "/home/gamer/.config/citra-emu"
+  #       "/home/gamer/.config/dolphin-emu"
+  #       "/home/gamer/.config/melonDS"
+  #       "/home/gamer/.config/mgba"
+  #       "/home/gamer/.config/PCSX2"
+  #       "/home/gamer/.config/ppsspp"
+  #       "/home/gamer/.config/retroarch"
+  #       "/home/gamer/.config/rpcs3"
+  #       "/home/gamer/.config/Ryujinx"
+  #       "/home/gamer/.config/sunshine"
+  #       "/home/gamer/.local/share/Cemu"
+  #       "/home/gamer/.local/share/citra-emu"
+  #       "/home/gamer/.local/share/dolphin-emu"
+  #       "/home/gamer/.local/share/duckstation"
+  #       "/home/gamer/.local/share/Steam"
+  #       "/home/gamer/.local/share/xemu"
+  #     )
 
-      for dir in "''${restore_dirs[@]}"; do
-        # Ensure the target directory exists
-        mkdir -p "$dir"
+  #     for dir in "''${restore_dirs[@]}"; do
+  #       # Ensure the target directory exists
+  #       mkdir -p "$dir"
         
-        # Restore files from temp_dir to the original location
-        ${pkgs.rsync}/bin/rsync -avhP "$temp_dir_extract/$dir/" "$dir"
-      done
+  #       # Restore files from temp_dir to the original location
+  #       ${pkgs.rsync}/bin/rsync -avhP "$temp_dir_extract/$dir/" "$dir"
+  #     done
 
-      # Clean up
-      ${pkgs.umount}/bin/umount $temp_dir_mount
-      rm -rf $temp_dir_mount
-      rm -rf $temp_dir_tar
-      rm -rf $temp_dir_extract
+  #     # Clean up
+  #     ${pkgs.umount}/bin/umount $temp_dir_mount
+  #     rm -rf $temp_dir_mount
+  #     rm -rf $temp_dir_tar
+  #     rm -rf $temp_dir_extract
 
-      echo "Restoration completed successfully."
-    '';
+  #     echo "Restoration completed successfully."
+  #   '';
 
-    serviceConfig = {
-      Type = "oneshot";
-    };
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #   };
     
-  };
+  # };
 
   systemd.services.rom-sync = {
     description = "Sync ROMs directory with NAS ";
