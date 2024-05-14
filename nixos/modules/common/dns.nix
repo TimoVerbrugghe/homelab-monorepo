1b8c2cc3d0886b71bed1874b8d72551f16b8de1b
Timo Verbrugghe <timo@hotmail.be>
2024-05-14 21:27:57 +0200

    Adding systemd-resolved to other configs as well

--- nixos/modules/common/dns.nix
+++ nixos/modules/common/dns.nix
@@ -7,4 +7,13 @@
     "10.10.10.21"
     "10.10.10.22"
   ];
+
+  # Add a dns manager so that tailscale does not overwrite /etc/resolv.conf. Ensures that docker dns keeps working
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [ "10.10.10.20" "10.10.10.21" "10.10.10.22" ];
+    dnsovertls = "true";
+  };
 }
