2541b79277ec18ead4ee6965bd4b3ab5594def23
Timo Verbrugghe <timo@hotmail.be>
2024-10-12 19:47:27 +0000

    chore: Update intel-gpu-drivers.nix to include onevpl-intel-gpu package and remove unused dependencies

--- nixos/modules/intel-gpu-drivers.nix
+++ nixos/modules/intel-gpu-drivers.nix
@@ -14,12 +14,18 @@
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
+      # Quick Sync Video
+      onevpl-intel-gpu
+      # vpl-gpu-rt after 24.11
+
+      # General Intel GPU iHD driver
       intel-media-driver
+
+      # OpenCL runtime
       intel-compute-runtime
-      vaapiVdpau
+
+      # Use VDPAU (normally only supported on NVIDIA/AMD gpus on intel gpus)
       libvdpau-va-gl
-      vaapiIntel
-      intel-ocl
     ];
   };
 }
