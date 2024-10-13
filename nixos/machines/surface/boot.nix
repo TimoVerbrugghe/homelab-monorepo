026780db2bec4b852031b27fad49df74fda16adf
TimoVerbrugghe <timo@hotmail.be>
2024-10-13 14:32:21 +0200

    feat: Enable plymouth and silent boot in surface NixOS configuration

--- nixos/machines/surface/boot.nix
+++ nixos/machines/surface/boot.nix
@@ -15,8 +15,34 @@
   boot.initrd = {
     supportedFilesystems = [ "nfs" ];
     kernelModules = [ "nfs" ];
+    verbose = false;
   };
 
   # Clean /tmp folder on reboot (apparantly false by default)
   boot.tmp.cleanOnBoot = true;
+
+  # Enable plymouth for a nice boot screen
+    boot = {
+
+    plymouth = {
+      enable = true;
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    # loader.timeout = 0;
+
+  };
 }
