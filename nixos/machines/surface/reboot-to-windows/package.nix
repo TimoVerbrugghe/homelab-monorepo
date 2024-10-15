4564d223e7f8375b2571a9ee01f2fc5e94550036
TimoVerbrugghe <timo@hotmail.be>
2024-10-15 10:20:42 +0200

    feat: Add reboot-to-windows package
    This commit adds a new package called "reboot-to-windows" which allows users to change the EFI boot order and reboot the system to Windows. The package includes a shell script that modifies the boot order and a desktop item for easy access. It also includes dependencies on efibootmgr and makeDesktopItem.
    
    Co-authored-by: timoverbrugghe

---
+++ nixos/machines/surface/reboot-to-windows/package.nix
@@ -0,0 +1,67 @@
+{ pkgs, stdenv, lib, makeDesktopItem, ...}:
+
+let
+  script = pkgs.writeShellScriptBin "reboot-to-windows" ''
+    #!/bin/sh
+    set -e
+
+    echo "Changing boot order to: Windows Boot Manager, PreLoader, Linux Boot Manager"
+    windows_boot=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "Windows Boot Manager" | grep -oP 'Boot\\K\\d+')
+    preloader_boot=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "PreLoader" | grep -oP 'Boot\\K\\d+')
+    linux_boot=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "Linux Boot Manager" | grep -oP 'Boot\\K\\d+')
+
+    boot_order="${windows_boot}"
+    if [ -n "$preloader_boot" ]; then
+      boot_order="${boot_order},${preloader_boot}"
+    fi
+    if [ -n "$linux_boot" ]; then
+      boot_order="${boot_order},${linux_boot}"
+    fi
+
+    ${pkgs.efibootmgr}/bin/efibootmgr -o ${boot_order} >/dev/null 2>&1
+
+    echo "Rebooting the system"
+    systemctl reboot
+  '';
+
+in
+
+stdenv.mkDerivation {
+  pname = "reboot-to-windows";
+  version = "1.0.0";
+
+  src = fs.toSource {
+    root = ./.;
+    fileset = sourceFiles;
+  };
+
+  buildInputs = [ pkgs.efibootmgr ];
+
+  desktopItem = makeDesktopItem {
+    name = "Reboot to Windows";
+    desktopName = "Reboot to Windows";
+    exec = "reboot-to-windows";
+    comment = "Reboot to Windows";
+    genericName = "Desktop application to quickly reboot to Windows";
+    categories = ["Utility"];
+  };
+
+  installPhase = ''
+    runHook PreInstall
+    mkdir -p $out/bin
+
+    # Install script
+    ln -s "${script}" $out/bin/reboot-to-windows.sh
+    chmod +x $out/bin/reboot-to-windows.sh
+
+    # Create Desktop Item
+    mkdir -p "$out/share/applications"
+    ln -s "${desktopItem}"/share/applications/* "$out/share/applications/"
+  '';
+
+  meta = with pkgs.lib; {
+    description = "A tool to change the EFI boot order and reboot the system";
+    license = licenses.mit;
+    maintainers = with maintainers; [ timoverbrugghe ];
+  };
+}
