abef7420eec0c91f394f718e7a1852afe7534e95
TimoVerbrugghe <timo@hotmail.be>
2024-10-15 18:54:15 +0200

    refactor: Remove unnecessary source file inclusion in reboot-to-windows package

--- nixos/machines/surface/reboot-to-windows/package.nix
+++ nixos/machines/surface/reboot-to-windows/package.nix
@@ -1,36 +1,6 @@
-{ pkgs, stdenv, lib, makeDesktopItem, ...}:
+{ pkgs, stdenv, efibootmgr, lib, makeDesktopItem, ...}:
 
 let
-  script = pkgs.writeShellScriptBin "reboot-to-windows" ''
-    #!/bin/sh
-    set -e
-
-    echo "Changing boot order to: Windows Boot Manager, PreLoader, Linux Boot Manager"
-    windows_boot=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "Windows Boot Manager" | grep -oP 'Boot\\K\\d+')
-    preloader_boot=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "PreLoader" | grep -oP 'Boot\\K\\d+')
-    linux_boot=$(${pkgs.efibootmgr}/bin/efibootmgr | grep -i "Linux Boot Manager" | grep -oP 'Boot\\K\\d+')
-
-    boot_order="${windows_boot}"
-    if [ -n "$preloader_boot" ]; then
-      boot_order="${boot_order},${preloader_boot}"
-    fi
-    if [ -n "$linux_boot" ]; then
-      boot_order="${boot_order},${linux_boot}"
-    fi
-
-    ${pkgs.efibootmgr}/bin/efibootmgr -o ${boot_order} >/dev/null 2>&1
-
-    echo "Rebooting the system"
-    systemctl reboot
-  '';
-
-in
-
-stdenv.mkDerivation {
-  pname = "reboot-to-windows";
-  version = "1.0.0";
-
-  buildInputs = [ pkgs.efibootmgr ];
 
   desktopItem = makeDesktopItem {
     name = "Reboot to Windows";
@@ -41,17 +11,51 @@ stdenv.mkDerivation {
     categories = ["Utility"];
   };
 
+in
+
+stdenv.mkDerivation {
+  pname = "reboot-to-windows";
+  version = "1.0.0";
+
+  buildInputs = [ pkgs.efibootmgr ];
+
+  src = pkgs.writeShellApplication {
+    name = "reboot-to-windows";
+    runtimeInputs = [ pkgs.efibootmgr ];
+    text = ''
+      #!/bin/sh
+      set -e
+
+      echo "Changing boot order to: Windows Boot Manager, PreLoader, Linux Boot Manager"
+      windows_boot=$(efibootmgr | grep -i "Windows Boot Manager" | grep -oP 'Boot\\K\\d+')
+      preloader_boot=$(efibootmgr | grep -i "PreLoader" | grep -oP 'Boot\\K\\d+')
+      linux_boot=$(efibootmgr | grep -i "Linux Boot Manager" | grep -oP 'Boot\\K\\d+')
+
+      boot_order="''${windows_boot}"
+      if [ -n "$preloader_boot" ]; then
+        boot_order="''${boot_order},''${preloader_boot}"
+      fi
+      if [ -n "$linux_boot" ]; then
+        boot_order="''${boot_order},''${linux_boot}"
+      fi
+
+      efibootmgr -o "''${boot_order}" >/dev/null 2>&1
+
+      # echo "Rebooting the system"
+      # systemctl reboot
+    '';
+  };
+    
+
   installPhase = ''
     runHook PreInstall
-    mkdir -p $out/bin
 
     # Install script
-    ln -s "${script}" $out/bin/reboot-to-windows.sh
-    chmod +x $out/bin/reboot-to-windows.sh
+    cp -a . $out
 
     # Create Desktop Item
     mkdir -p "$out/share/applications"
-    ln -s "${desktopItem}"/share/applications/* "$out/share/applications/"
+    cp -a ${desktopItem} $out/share/applications/
   '';
 
   meta = with pkgs.lib; {
