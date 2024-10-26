0153079890af28cb1dc847263ddfe79d50a51b9b
TimoVerbrugghe <timo@hotmail.be>
2024-10-26 11:16:49 +0200

    Update startupWMClass for iCloud Reminders and iCloud Photos desktop items in Chrome apps

--- nixos/machines/surface/chrome-apps/package.nix
+++ nixos/machines/surface/chrome-apps/package.nix
@@ -127,7 +127,7 @@ stdenv.mkDerivation {
       genericName = "Launch iCloud Reminders";
       categories = ["Utility"];
       icon = ./reminders.png;
-      startupWMClass = "chrome-wwww.icloud.com__reminders__-Default";
+      startupWMClass = "chrome-wwww.icloud.com__reminders_-Default";
     })
 
     (makeDesktopItem {
@@ -138,7 +138,7 @@ stdenv.mkDerivation {
       genericName = "Launch iCloud Photos";
       categories = ["Utility"];
       icon = ./photos.svg;
-      startupWMClass = "chrome-www.icloud.com__photos__-Default";
+      startupWMClass = "chrome-www.icloud.com__photos_-Default";
     })
 
   ];
