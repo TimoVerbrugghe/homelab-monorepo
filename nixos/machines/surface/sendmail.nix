542a34e55638bc060eab6ba83f5e8f16231474a3
TimoVerbrugghe <timo@hotmail.be>
2024-12-01 21:46:51 +0100

    Refactor Postfix configuration to use a structured format

--- nixos/machines/surface/sendmail.nix
+++ nixos/machines/surface/sendmail.nix
@@ -4,15 +4,15 @@
   services.postfix = {
     enable = true;
     rootAlias = "root";
-    config = ''
-      relayhost = [smtp.gmail.com]:587
-      smtp_use_tls = yes
-      smtp_sasl_auth_enable = yes
-      smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
-      smtp_sasl_security_options = noanonymous
-      smtp_tls_security_level = encrypt
-      smtp_tls_note_starttls_offer = yes
-    '';
+    config = {
+      relayhost = "[smtp.gmail.com]:587";
+      smtp_use_tls = "yes";
+      smtp_sasl_auth_enable = "yes";
+      smtp_sasl_password_maps = "hash:/etc/postfix/sasl_passwd";
+      smtp_sasl_security_options = "noanonymous";
+      smtp_tls_security_level = "encrypt";
+      smtp_tls_note_starttls_offer = "yes";
+    };
   };
 
   environment.etc."postfix/sasl_passwd" = {
