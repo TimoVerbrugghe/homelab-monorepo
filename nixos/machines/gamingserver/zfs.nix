4b9804cdddc0ea69e15cdd7f93d8291a7e6992ac
TimoVerbrugghe <timo@hotmail.be>
2024-12-24 12:46:58 +0100

    Remove kernelPackages setting for ZFS in gaming server configuration

--- nixos/machines/gamingserver/zfs.nix
+++ nixos/machines/gamingserver/zfs.nix
@@ -10,8 +10,6 @@
   # Set networking.hostId because ZFS needs this to prevent zfs pool from being mounted on the wrong machine. Host id is derived from the systemd machine id (head -c 8 /etc/machine-id)
   networking.hostId = "e6b5d6a5";
 
-  boot.kernelPackages = pkgs.zfs.latestCompatibleLinuxPackages; # Making sure we're running latest linux kernel that is ZFS compatible
-
   # Mount filesystems
   fileSystems."/" =
     { device = "gamingpool/root";
