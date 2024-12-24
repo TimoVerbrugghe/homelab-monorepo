fc1288e3773f6294d44d8f2b4da8ea933136da6b
TimoVerbrugghe <timo@hotmail.be>
2024-12-24 14:40:46 +0100

    Fix Exec command substitution in Citra and Ryujinx package definitions to ensure correct execution of AppImage

--- nixos/machines/gamingserver/ryujinx/package.nix
+++ nixos/machines/gamingserver/ryujinx/package.nix
@@ -20,7 +20,7 @@ appimageTools.wrapType2 {
   extraInstallCommands = ''
     install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=AppRun' 'Exec=${pname}'
+      --replace-fail 'Exec=' 'Exec=${pname}'
     cp -r ${appimageContents}/usr/share/icons $out/share
   '';
 
