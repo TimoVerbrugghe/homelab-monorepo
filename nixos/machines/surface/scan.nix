55f56994ae8f6ec51261b10f8d1e3ce35c196df1
TimoVerbrugghe <timo@hotmail.be>
2025-01-02 16:52:20 +0100

    Update scanner configuration to enable brscan5 and remove brscan4 settings

--- nixos/machines/surface/scan.nix
+++ nixos/machines/surface/scan.nix
@@ -1,19 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  # imports = [
-  #   <nixpkgs/nixos/modules/services/hardware/sane_extra_backends/brscan4.nix>
-  # ];
-
   hardware = {
     sane = {
       enable = true;
-      # brscan4 = {
-      #   # enable = true;
-      #   netDevices = {
-      #     home = { model = "DCP-L2627DWE"; ip = "10.10.10.197"; };
-      #   };
-      # };
+      brscan5 = {
+        enable = true;
+        netDevices = {
+          home = { model = "DCP-L2627DWE"; ip = "10.10.10.197"; };
+        };
+      };
     };
   };
 }
