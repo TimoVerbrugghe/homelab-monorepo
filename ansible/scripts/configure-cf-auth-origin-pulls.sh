3a429983d4c11ac96d72e14fdf95563ad803036e
TimoVerbrugghe <timo@hotmail.be>
2025-01-04 15:59:35 +0100

    Cleanup

--- ansible/scripts/configure-cf-auth-origin-pulls.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ansible-playbook \
-    -i ../inventory/hosts.yaml \
-    --limit aelita.home.timo.be \
-    ../playbooks/cf-auth-origin-pulls.yaml
--- ansible/scripts/configure-nixos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ansible-playbook -vvv \
-     -i "10.10.10.23," \
-     -e "tailscaleAuthkey=" \
-     -e "nixosFlake=nixos" \
-     -e "ansible_username=nixos" \
-     -e "ansible_password=nixos" \
-     -e "ansible_become_password=nixos" \
-     ../playbooks/configure-nixos.yaml
