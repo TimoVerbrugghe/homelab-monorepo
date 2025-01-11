4e6d055c04f43b2a6657db5dec1285a27aeb1ec7
TimoVerbrugghe <timo@hotmail.be>
2025-01-11 21:13:54 +0100

    Add Intel GPU setup for Proxmox with cloudflared service installation and updated playbook limits

--- ansible/scripts/configure-proxmox-host.sh
+++ ansible/scripts/configure-proxmox-host.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ansible-playbook \
      -i ../inventory/hosts.yaml \
-     --limit localhost,proxmox\
+     --limit localhost,icesector.local.timo.be \
      ../playbooks/proxmox-host-setup.yaml
