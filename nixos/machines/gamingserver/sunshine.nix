6a527af9f8b9fb4640cbd6ed4c01903b9240269e
TimoVerbrugghe <timo@hotmail.be>
2025-01-19 01:36:35 +0100

    Refactor sunshine service configuration in gamingserver; enable service and adjust security settings

--- nixos/machines/gamingserver/sunshine.nix
+++ nixos/machines/gamingserver/sunshine.nix
@@ -6,39 +6,14 @@
     "uinput"
   ];
 
-  environment.systemPackages = with pkgs; [
-    sunshine
-  ];
-
-  # In wayland, sunshine needs additional security capabilities to detect displays
-  security.wrappers.sunshine = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_sys_admin+p";
-      source = "${pkgs.sunshine}/bin/sunshine";
+  services.sunshine = {
+    enable = true;
+    capSysAdmin = true;
+    openFirewall = true;
+    autoStart = true;
   };
-
-
-  # Because of the additional security capabilities, path of sunshine in systemd service has to be changed
-  systemd.user.services.sunshine =
-    {
-      description = "sunshine";
-      wantedBy = [ "graphical-session.target" ];
-      serviceConfig = {
-        ExecStart = "${config.security.wrapperDir}/sunshine";
-      };
-    };
 
   # Needed for autodetection of sunshine in moonlight
   services.avahi.publish.enable = true;
   services.avahi.publish.userServices = true;
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 47984 47989 47990 48010 ];
-    allowedUDPPortRanges = [
-      { from = 47998; to = 48000; }
-    ];
-  };
-
 }
