e75a724c74a29c30b9ffd2617d49ade441cf9399
TimoVerbrugghe <timo@hotmail.be>
2025-02-08 13:24:26 +0100

    Add common variables module to Azure NixOS configuration

--- nixos/machines/azurenixos/configuration.nix
+++ nixos/machines/azurenixos/configuration.nix
@@ -17,6 +17,7 @@ in
     ../../modules/common/packages.nix # Add default packages
     ../../modules/tailscale.nix # Common tailscale config options, you need to add a tailscale authkey file to /etc/nixos/tailscale-authkey.nix
     ../../modules/remote-builder.nix # Enable using this machine as remote builder for nixos
+    ../../modules/common/vars.nix # Add common variables
   ];
 
   ############################
