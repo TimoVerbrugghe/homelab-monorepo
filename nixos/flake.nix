be93a495999fd6e33de67e1edbbe5a3520ccfabc
TimoVerbrugghe <timo@hotmail.be>
2025-02-08 13:23:40 +0100

    Add specialArgs to azurenixos generation for improved input handling

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -170,6 +170,7 @@
       # Build this Azure VHD image with nix build github:TimoVerbrugghe/homelab-monorepo?dir=nixos#azurenixos --no-write-lock-file --refresh
       azurenixos = nixos-generators.nixosGenerate {
         system = "x86_64-linux";
+        specialArgs = inputs;
         modules = [
           ./machines/azurenixos/configuration.nix
           {
