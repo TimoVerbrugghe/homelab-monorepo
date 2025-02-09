6a47618cd83eda08e9cd150112ed2293c7050f7f
Timo Verbrugghe <timo@hotmail.be>
2025-02-09 23:00:14 +0100

    Update Proxmox host setup script to remove redundant host limit

--- ansible/scripts/configure-proxmox-host.sh
+++ ansible/scripts/configure-proxmox-host.sh
@@ -2,5 +2,4 @@
 ansible-playbook \
      -i ../inventory/hosts.yaml \
      --limit localhost,forestsector.local.timo.be \
-     --limit localhost,icesector.local.timo.be \
      ../playbooks/proxmox-host-setup.yaml
