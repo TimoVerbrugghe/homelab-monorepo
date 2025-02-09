ef0eced13ffc45cee0e5d2f6dc42542afe9506b9
Timo Verbrugghe <timo@hotmail.be>
2025-02-09 22:59:18 +0100

    Update Proxmox host setup script to limit playbook execution to specific host

--- ansible/scripts/configure-proxmox-host.sh
+++ ansible/scripts/configure-proxmox-host.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ansible-playbook \
      -i ../inventory/hosts.yaml \
-     --limit localhost,proxmox\
+     --limit localhost,forestsector.local.timo.be \
      ../playbooks/proxmox-host-setup.yaml
