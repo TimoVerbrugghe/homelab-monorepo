22e5979fa19c0fdca6caf2d2fea160936514bdaf
Timo Verbrugghe <timo@hotmail.be>
2025-02-16 00:44:57 +0100

    Update emulators.nix

--- nixos/machines/gamingserver/emulators.nix
+++ nixos/machines/gamingserver/emulators.nix
@@ -4,7 +4,7 @@
   nixpkgs.overlays = [
     (final: _: {
       # this allows you to access `pkgs.unstable` anywhere in your config
-      unstable = import nixpkgs-unstable {
+      unstable = import inputs.nixpkgs-unstable {
         inherit (final.stdenv.hostPlatform) system;
         inherit (final) config;
       };
