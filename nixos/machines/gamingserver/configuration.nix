4552e5fb075215ca35971505396d97e951f6dffb
Timo Verbrugghe <timo@hotmail.be>
2025-02-16 01:13:49 +0100

    Update configuration.nix

--- nixos/machines/gamingserver/configuration.nix
+++ nixos/machines/gamingserver/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, ssh-keys, ... }:
 
 let 
 
@@ -76,7 +76,7 @@ in
       isNormalUser = true;
       createHome = true;
       hashedPassword = "${hashedPassword}";
-      openssh.authorizedKeys.keyFiles = [ inputs.ssh-keys.outPath ];
+      openssh.authorizedKeys.keyFiles = [ ssh-keys.outPath ];
     };
 
     root = {
