0b341dfcd50e2fd724944d611555ff16809c31ae
Timo Verbrugghe <timo@hotmail.be>
2025-02-16 01:35:59 +0100

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -135,7 +135,7 @@
       };
 
       # Switch to this config (for the next boot) with nixos-rebuild boot --flake github:TimoVerbrugghe/homelab-monorepo?dir=nixos#gamingserver --refresh --impure --no-write-lock-file
-      gamingserver = nixpkgs.lib.nixosSystem {
+      gamingserver = nixpkgs-unstable.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs;
         modules = [
