c6daa7defe1a75b406791ab90a4a195cfd30eb92
TimoVerbrugghe <timo@hotmail.be>
2025-02-18 23:37:38 +0100

    Add Traefik ingress class annotations and remove longhorn ingress configuration

--- nixos/machines/gamingserver/sleep.nix
+++ nixos/machines/gamingserver/sleep.nix
@@ -21,4 +21,22 @@
     }
   ];
 
+  # # Autosleep at 3 am (if I forget to put the computer manually to sleep)
+  # systemd.services.sleepAtFour = {
+  #   description = "Put the machine to sleep at 4 am";
+  #   wantedBy = [ "timers.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "${pkgs.systemd}/bin/systemctl suspend";
+  #   };
+  # };
+
+  # systemd.timers.sleepAtFourTimer = {
+  #   description = "Timer to put the machine to sleep at 4 am";
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig = {
+  #     OnCalendar = "*-*-* 04:00:00";
+  #     Persistent = true;  # Ensure the timer isn't missed if the machine was asleep at the scheduled time
+  #   };
+  # };
 }
