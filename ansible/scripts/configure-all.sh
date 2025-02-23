44d071bf50506474cdaf131694dbc5c1987b446f
TimoVerbrugghe <timo@hotmail.be>
2025-02-23 22:43:37 +0100

    Cleanup since switching to nixos & talos linux for kubernetes

--- ansible/scripts/configure-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Do a full configuration across several proxmox nodes & setup Aelita VM
-
-ansible-playbook \
-     -i ../inventory/hosts.yaml \
-     ../playbooks/full-proxmox-setup.yaml
--- ansible/scripts/deploy-k3scluster-vm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ansible-playbook \
-    -i ../inventory/hosts.yaml \
-    --limit k3stest.local.timo.be,localhost \
-    ../playbooks/deploy-k3scluster.yaml
