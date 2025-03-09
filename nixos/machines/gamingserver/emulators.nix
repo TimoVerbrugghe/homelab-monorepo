542d6dd32177f964f874d155e68ab503752cb3ee
TimoVerbrugghe <timo@hotmail.be>
2025-03-09 18:44:20 +0100

    Enhance emulationstation-de configuration to include stable libgit2 and ICU, add necessary compiler flags, and improve pkg-config environment setup

--- nixos/machines/gamingserver/emulators.nix
+++ nixos/machines/gamingserver/emulators.nix
@@ -2,43 +2,60 @@
 
 {
   
-  nixpkgs.overlays = [
-    (final: prev: {
-      # Import stable channel
-      stable = import nixpkgs {
-        inherit (final.stdenv.hostPlatform) system;
-        inherit (final) config;
-      };
+nixpkgs.overlays = [
+  (final: prev: {
+    # Import stable channel
+    stable = import nixpkgs {
+      inherit (final.stdenv.hostPlatform) system;
+      inherit (final) config;
+    };
+    
+    # Create a fixed version of emulationstation-de
+    emulationstation-de = prev.emulationstation-de.overrideAttrs (oldAttrs: {
+      # Add stable dev packages for headers
+      nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
+        final.stable.pkg-config
+        final.stable.sysprof # For sysprof-capture-4
+      ];
       
-      # Create a fixed version of emulationstation-de
-      emulationstation-de = prev.emulationstation-de.overrideAttrs (oldAttrs: {
-        # Use both stable libgit2 and stable icu
-        buildInputs = let
-          # Use prev.lib to access the lib function
-          filteredInputs = builtins.filter (input: 
-            !(prev.lib.hasPrefix "libgit2" (input.name or "")) && 
-            !(prev.lib.hasPrefix "icu" (input.name or ""))
-          ) (oldAttrs.buildInputs or []);
-        in
-          filteredInputs ++ [ 
-            final.stable.libgit2
-            final.stable.icu
-          ];
-        
-        # Add necessary compiler flags to work around the nullptr comparison issue
-        NIX_CFLAGS_COMPILE = (oldAttrs.NIX_CFLAGS_COMPILE or "") + " -fpermissive";
-        
-        # Make sure CMake finds the stable versions
-        cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
-          "-DLIBGIT2_INCLUDE_DIR=${final.stable.libgit2}/include"
-          "-DLIBGIT2_LIBRARIES=${final.stable.libgit2}/lib/libgit2${final.stdenv.hostPlatform.extensions.sharedLibrary}"
-          "-DICU_INCLUDE_DIR=${final.stable.icu}/include"
-          "-DICU_LIBRARY=${final.stable.icu}/lib"
+      # Use both stable libgit2 and stable icu
+      buildInputs = let
+        filteredInputs = builtins.filter (input: 
+          !(prev.lib.hasPrefix "libgit2" (input.name or "")) && 
+          !(prev.lib.hasPrefix "icu" (input.name or ""))
+        ) (oldAttrs.buildInputs or []);
+      in
+        filteredInputs ++ [ 
+          final.stable.libgit2
+          # Add both runtime and dev versions of ICU
+          final.stable.icu
+          (final.stable.icu.override { withDev = true; })
+          # Add glib with sysprof explicitly
+          final.stable.glib
         ];
-      });
-    })
-  ];
-
+      
+      # Add necessary compiler flags
+      NIX_CFLAGS_COMPILE = (oldAttrs.NIX_CFLAGS_COMPILE or "") + " -fpermissive";
+      
+      # Point to ICU dev files explicitly
+      cmakeFlags = (oldAttrs.cmakeFlags or []) ++ [
+        "-DLIBGIT2_INCLUDE_DIR=${final.stable.libgit2}/include"
+        "-DLIBGIT2_LIBRARIES=${final.stable.libgit2}/lib/libgit2${final.stdenv.hostPlatform.extensions.sharedLibrary}"
+        "-DICU_ROOT=${final.stable.icu.dev}"
+        "-DICU_INCLUDE_DIR=${final.stable.icu.dev}/include"
+        "-DICU_LIBRARY_DIR=${final.stable.icu}/lib"
+        "-DICU_I18N_LIBRARY_RELEASE=${final.stable.icu}/lib/libicui18n${final.stdenv.hostPlatform.extensions.sharedLibrary}"
+        "-DICU_UC_LIBRARY_RELEASE=${final.stable.icu}/lib/libicuuc${final.stdenv.hostPlatform.extensions.sharedLibrary}"
+        "-DICU_DATA_LIBRARY_RELEASE=${final.stable.icu}/lib/libicudata${final.stdenv.hostPlatform.extensions.sharedLibrary}"
+      ];
+      
+      # Add environment variables for pkg-config to find the right dependencies
+      preConfigure = (oldAttrs.preConfigure or "") + ''
+        export PKG_CONFIG_PATH="${final.stable.sysprof}/lib/pkgconfig:${final.stable.glib.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+      '';
+    });
+  })
+];
 
   environment.systemPackages = with pkgs; [
     retroarch-full
