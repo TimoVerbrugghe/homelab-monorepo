2880ab67fe0977ad6c2db8f701587a5728d386db
Timo Verbrugghe <timo@hotmail.be>
2025-03-29 22:42:08 +0100

    Update version to 0.28 and adjust resource copying in Vapor Nix theme package

--- nixos/machines/gamingserver/vapor-nixos-theme/package.nix
+++ nixos/machines/gamingserver/vapor-nixos-theme/package.nix
@@ -2,7 +2,7 @@
 
 let
 
-  version = "0.27";
+  version = "0.28";
 
 in
 
@@ -12,8 +12,7 @@ stdenv.mkDerivation rec {
   
   src = fetchurl {
     url = "https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steamdeck-kde-presets-${version}-1-any.pkg.tar.zst";
-    sha256 = "1189zj55qcc3xh3n4j9dxjcrqa7qgbl29wkcgxkxqz92gvwb9ll0"; # Update this if needed for 0.27
-    # sha256 = "0mgdkcq9z4r9wlwl02ihrigg3hay5kn7475vqmqgs13nvpy22000"; # Update this if needed for 0.28
+    sha256 = "0mgdkcq9z4r9wlwl02ihrigg3hay5kn7475vqmqgs13nvpy22000"; # Update this when updating version using nix-prefetch-url
   };
 
   # Add zstd to nativeBuildInputs for extraction
@@ -40,21 +39,14 @@ stdenv.mkDerivation rec {
     mkdir -p $out/share/themes
     mkdir -p $out/share/plasma/avatars
 
-    # Copy and modify desktop theme
-    cp -r source/usr/share/plasma/desktoptheme/Vapor/* $out/share/plasma/desktoptheme/Vapor/
-
-    # Setup look-and-feel component
-    cp -r source/usr/share/plasma/look-and-feel/com.valve.vapor.desktop/* $out/share/plasma/look-and-feel/Vapor/
-    
-    # Copy color schemes
-    cp source/usr/share/plasma/desktoptheme/Vapor/colors $out/share/plasma/desktoptheme/Vapor/colors
-    cp source/usr/share/color-schemes/Vapor.colors $out/share/color-schemes/Vapor.colors
-
-    # Copy additional resources from the source package
+    # Copy resources from the source theme package
+    [ -d source/usr/share/plasma/look-and-feel ] && cp -r source/usr/share/plasma/look-and-feel/* $out/share/plasma/look-and-feel/
+    [ -d source/usr/share/plasma/desktoptheme ] && cp -r source/usr/share/plasma/desktoptheme/* $out/share/plasma/desktoptheme/
+    [ -d source/usr/share/color-schemes ] && cp -r source/usr/share/color-schemes/* $out/share/color-schemes/
     [ -d source/usr/share/wallpapers ] && cp -r source/usr/share/wallpapers/* $out/share/wallpapers/
     [ -d source/usr/share/konsole ] && cp -r source/usr/share/konsole/* $out/share/konsole/
-    [ -d source/usr/share/icons/hicolor ] && cp -r source/usr/share/icons/hicolor $out/share/icons/
-    [ -d source/usr/share/themes/Vapor ] && cp -r source/usr/share/themes/Vapor $out/share/themes/
+    [ -d source/usr/share/icons ] && cp -r source/usr/share/icons/* $out/share/icons/
+    [ -d source/usr/share/themes ] && cp -r source/usr/share/themes/* $out/share/themes/
     [ -d source/usr/share/plasma/avatars ] && cp -r source/usr/share/plasma/avatars/* $out/share/plasma/avatars/
     
     runHook postInstall
