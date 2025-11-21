77ba31109950fa6adbaa4a44da24d36ad96ee046
Timo Verbrugghe <timo@hotmail.be>
2025-11-21 10:41:26 +0100

    Add S.M.A.R.T. checks and Docker cleanup to cronjobs script

--- truenas/cronjobs.sh
+++ truenas/cronjobs.sh
@@ -1,2 +1,11 @@
 ## Backup Truenas Config
 /mnt/X.A.N.A./truenasconfig-backup/backupscript.sh
+
+# Short S.M.A.R.T. check on all devices (every day)
+midclt call disk.smart_test SHORT '["*"]'
+
+# Long S.M.A.R.T. check on all devices (every 4 months)
+midclt call disk.smart_test LONG '["*"]'
+
+# Clean up docker resources (orphaned images, unused networks, etc...)
+docker system prune -a -f
