fbb5bc43464f8ea9f2d4c5637c014eb080d6f114
Timo Verbrugghe <timo@hotmail.be>
2025-12-07 21:11:55 +0100

    Fix iSCSI IQN format in storage and target configurations

--- nixos/modules/iscsi-target.nix
+++ nixos/modules/iscsi-target.nix
@@ -27,10 +27,10 @@
       # Configure target using targetcli
       ${pkgs.targetcli}/bin/targetcli <<EOF
         /backstores/fileio create iscsi-test /home/odd/iscsi/disk.img 10G
-        /iscsi create iqn.2025-12.be.timo.local:iscsi-test
-        /iscsi/iqn.2025-12.be.timo.local:iscsi-test/tpg1/luns create /backstores/fileio/iscsi-test
-        /iscsi/iqn.2025-12.be.timo.local:iscsi-test/tpg1/acls create iqn.2005-03.org.open-iscsi:*
-        /iscsi/iqn.2025-12.be.timo.local:iscsi-test/tpg1 set attribute authentication=0 demo_mode_write_protect=0 generate_node_acls=1 cache_dynamic_acls=1
+        /iscsi create iqn.2025-12.be.timo.local.odd:iscsi-test
+        /iscsi/iqn.2025-12.be.timo.local.odd:iscsi-test/tpg1/luns create /backstores/fileio/iscsi-test
+        /iscsi/iqn.2025-12.be.timo.local.odd:iscsi-test/tpg1/acls create iqn.2005-03.org.open-iscsi:*
+        /iscsi/iqn.2025-12.be.timo.local.odd:iscsi-test/tpg1 set attribute authentication=0 demo_mode_write_protect=0 generate_node_acls=1 cache_dynamic_acls=1
         saveconfig
         exit
       EOF
