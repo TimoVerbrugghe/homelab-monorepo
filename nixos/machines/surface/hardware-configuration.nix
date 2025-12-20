7cf6bd5362c2345b3cdf5784c8a77681df5b81dc
TimoVerbrugghe <timo@hotmail.be>
2025-12-20 11:26:02 +0100

    Switching surface kernel to stable from default LTS

--- nixos/machines/surface/hardware-configuration.nix
+++ nixos/machines/surface/hardware-configuration.nix
@@ -38,4 +38,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.microsoft-surface.kernelVersion = "stable";
+
 }
