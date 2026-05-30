725d6d3ddfb012983cae9b3172c8c612b652355e
Timo Verbrugghe <timo@hotmail.be>
2026-05-30 18:33:53 +0200

    Update Bazarr image tag and upgrade Nixpkgs and Home Manager versions

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.11";
+      url = "github:NixOS/nixpkgs/nixos-26.05";
     };
     
     nixpkgs-unstable = { 
@@ -16,7 +16,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
